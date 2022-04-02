2a36525a3c093924c481e7efc81789b082471519
Alexander Morogov <asmorogov@gmail.com>
2022-04-02 22:29:28 +0200

    Change V1Time

--- model_v1_time.go
+++ model_v1_time.go
@@ -10,9 +10,11 @@
 package swagger
 
 // Time is a wrapper around time.Time which supports correct marshaling to YAML and JSON.  Wrappers are provided for many of the factory methods that the time package offers.  +protobuf.options.marshal=false +protobuf.as=Timestamp +protobuf.options.(gogoproto.goproto_stringer)=false
-type V1Time struct {
-	// Non-negative fractions of a second at nanosecond resolution. Negative second values with fractions must still have non-negative nanos values that count forward in time. Must be from 0 to 999,999,999 inclusive. This field may be limited in precision depending on context.
-	Nanos int32 `json:"nanos,omitempty"`
-	// Represents seconds of UTC time since Unix epoch 1970-01-01T00:00:00Z. Must be from 0001-01-01T00:00:00Z to 9999-12-31T23:59:59Z inclusive.
-	Seconds string `json:"seconds,omitempty"`
-}
+type V1Time string
+
+// {
+// 	// Non-negative fractions of a second at nanosecond resolution. Negative second values with fractions must still have non-negative nanos values that count forward in time. Must be from 0 to 999,999,999 inclusive. This field may be limited in precision depending on context.
+// 	Nanos int32 `json:"nanos,omitempty"`
+// 	// Represents seconds of UTC time since Unix epoch 1970-01-01T00:00:00Z. Must be from 0001-01-01T00:00:00Z to 9999-12-31T23:59:59Z inclusive.
+// 	Seconds string `json:"seconds,omitempty"`
+// }
