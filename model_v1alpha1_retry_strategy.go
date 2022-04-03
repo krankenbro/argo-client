86097f48c2a7f92ba19558f3d29b6b645a2fb568
Alexander Morogov <asmorogov@gmail.com>
2022-04-03 17:19:50 +0200

    Fix type of Limit in V1alpha1RetryStrategy

--- model_v1alpha1_retry_strategy.go
+++ model_v1alpha1_retry_strategy.go
@@ -12,5 +12,5 @@ package swagger
 type V1alpha1RetryStrategy struct {
 	Backoff *V1alpha1Backoff `json:"backoff,omitempty"`
 	// Limit is the maximum number of attempts for retrying a failed sync. If set to 0, no retries will be performed.
-	Limit string `json:"limit,omitempty"`
+	Limit *string `json:"limit,omitempty"`
 }
